024a2cdf2688b4e0bd2766e64a51d9a682caa06b
kevin viglucci <kviglucci@gmail.com>
2019-03-31 12:41:05 -0500

    POC for rendering simple dom structure without JSX

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Title from "./Title";
+
+const App = () => {
+    return (
+        <Header>
+            <Title text="Hello World" />
+        </Header>
+    );
+};
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,7 @@
+const Title = ({
+    text
+}) => {
+  return <h1>{text}</h1>;
+};
+
+export default Title;
